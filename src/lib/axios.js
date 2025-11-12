bc2eb63d67e7ecc7ad6d09fe9c23209335bc4eaa
manas467 <137690405+manas467@users.noreply.github.com>
2025-11-13 04:25:12 +0530

    ready for deployment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  withCredentials: true 
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://finance-tracker-2-bbko.onrender.com"
+      : "http://localhost:5000",
+  withCredentials: true,
 });
-export default axiosInstance
+
+export default axiosInstance;
