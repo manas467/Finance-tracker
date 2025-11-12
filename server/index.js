bb794d075bd5caba35454c729246c174e036dba0
manas467 <137690405+manas467@users.noreply.github.com>
2025-11-13 04:58:27 +0530

    fix: corrected CORS origin URL

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://finance-tracker-amber-two.vercel.app/"
+    "https://finance-tracker-amber-two.vercel.app"
   ],
   credentials: true,
 }));
